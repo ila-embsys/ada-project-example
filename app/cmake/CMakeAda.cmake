e7ab13ae87e4a7100128898362dd6e5a615703f0
ila-embsys <ila.embsys@gmail.com>
2022-07-28 00:30:38 +0000

    Fix automatic build gprproject as dependensy

--- app/cmake/CMakeAda.cmake
+++ app/cmake/CMakeAda.cmake
@@ -7,13 +7,14 @@ cmake_minimum_required(VERSION 3.5)
 # LIBFILE must not have prefix nor suffix, and is expected to be static: libadatest.a -> adatest
 function(add_ada_library TARGET GPRFILE OUTPUT_REL_DIR)
 
-    # Actual library file
-    set(_result ${CMAKE_CURRENT_BINARY_DIR}/${OUTPUT_REL_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}${TARGET}${CMAKE_STATIC_LIBRARY_SUFFIX})
+# Actual library file
+set(_result ${CMAKE_CURRENT_BINARY_DIR}/${OUTPUT_REL_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}${TARGET}${CMAKE_STATIC_LIBRARY_SUFFIX})
 
-    # Build and leave Ada dependencies to gprbuild
+# Build and leave Ada dependencies to gprbuild
     add_custom_target(${TARGET}_build
         COMMAND gprbuild -p -P${CMAKE_SOURCE_DIR}/${GPRFILE} --relocate-build-tree=${CMAKE_CURRENT_BINARY_DIR} -cargs ${TOOLCHAIN_C_FLAGS}
         COMMENT "Building library ${TARGET}..."
+        BYPRODUCTS ${_result}
     )
 
     # Fake library target that depends on gprbuild result
